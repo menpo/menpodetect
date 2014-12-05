b03bc28da7476ca27e64b8cc01b685e11eb6d505
Patrick Snape <patricksnape@gmail.com>
2014-12-05 10:37:01 +0000

    Fix the circle to rectangle code
    Was totally incorrect previously

--- menpodetect/pico/conversion.py
+++ menpodetect/pico/conversion.py
@@ -3,10 +3,13 @@ import numpy as np
 
 
 def pointgraph_from_circle(fitting):
+    diameter = fitting.diameter
+    radius = diameter / 2.0
     y, x = fitting.center
-    radius = fitting.diameter / 2.0
+    y -= radius
+    x -= radius
     return PointDirectedGraph(np.array(((y, x),
-                                        (y + radius, x),
-                                        (y + radius, x + radius),
-                                        (y, x + radius))),
+                                        (y + diameter, x),
+                                        (y + diameter, x + diameter),
+                                        (y, x + diameter))),
                               np.array([[0, 1], [1, 2], [2, 3], [3, 0]]))

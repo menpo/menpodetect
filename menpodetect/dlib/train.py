37f1c1e2e71166aac6e6b09cec0e86b95454f4d4
Patrick Snape <patricksnape@gmail.com>
2014-12-05 10:37:22 +0000

    Expose training code for dlib
    Change the interface slightly to not return anything. Change the
    named of the verbose flag

--- menpodetect/dlib/train.py
+++ menpodetect/dlib/train.py
@@ -5,7 +5,7 @@ from .conversion import pointgraph_to_rect
 
 
 def train_dlib_detector(images, output_path, epsilon=0.01,
-                        add_left_right_image_flips=False, be_verbose=True,
+                        add_left_right_image_flips=False, verbose_stdout=False,
                         C=5, detection_window_size=6400, num_threads=None):
     rectangles = [[pointgraph_to_rect(lgroup.lms.bounding_box())
                    for lgroup in im.landmarks.values()]
@@ -20,7 +20,7 @@ def train_dlib_detector(images, output_path, epsilon=0.01,
     options = dlib.simple_object_detector_training_options()
     options.epsilon = epsilon
     options.add_left_right_image_flips = add_left_right_image_flips
-    options.be_verbose = be_verbose
+    options.be_verbose = verbose_stdout
     options.C = C
     options.detection_window_size = detection_window_size
     options.num_threads = num_threads
@@ -28,4 +28,3 @@ def train_dlib_detector(images, output_path, epsilon=0.01,
     output_path_str = str(output_path)
     dlib.train_simple_object_detector(images, rectangles, output_path_str,
                                       options)
-    return dlib.simple_object_detector(output_path_str)

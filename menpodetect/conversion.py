3dac8a55866fd5c605adf76ee45b4ada654c8ed5
James Booth <jabooth@gmail.com>
2015-04-12 18:30:18 +0100

    squash bug introduced by graph API changes
    See https://github.com/menpo/menpo/pull/559

--- menpodetect/conversion.py
+++ menpodetect/conversion.py
@@ -21,7 +21,7 @@ def bounding_box(min_point, max_point):
     bounding_box : :map:`PointDirectedGraph`
         The axis aligned bounding box from the given points.
     """
-    return PointDirectedGraph(
+    return PointDirectedGraph.init_from_edges(
         np.array([min_point, [max_point[0], min_point[1]],
                   max_point, [min_point[0], max_point[1]]]),
         np.array([[0, 1], [1, 2], [2, 3], [3, 0]]), copy=False)

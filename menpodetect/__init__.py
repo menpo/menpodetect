5981afbfcd7101e9f5f7eff06f498bc3da405e29
Patrick Snape <patricksnape@gmail.com>
2021-01-24 11:26:16 +0100

    Fix menpo version in setup.py
    Also move to miniver rather than versioneer

--- menpodetect/__init__.py
+++ menpodetect/__init__.py
@@ -1,7 +1,4 @@
 from menpodetect.dlib import *
 from menpodetect.opencv import *
 
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
+from ._version import __version__
